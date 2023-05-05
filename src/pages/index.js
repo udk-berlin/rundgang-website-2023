040edad7e90e364fc997ccee7c041939121560c9
marisanest <marisanest@mailbox.org>
2023-05-05 12:46:36 +0200

    Next.js initialization for version 13.2.4

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>UdK Rundgang 2023</title>
+        <meta name="description" content="UdK Rundgang 2023" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <div>Hello world!</div>
+      </main>
+    </>
+  )
+}
