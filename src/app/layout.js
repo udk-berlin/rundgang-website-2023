bf21f35146f30f5c6648c934b8292b025c0cbd34
marisanest <marisanest@mailbox.org>
2023-05-05 12:27:43 +0200

    Next.js initialization

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'UdK Rundgang 2023',
+  description: 'UdK Rundgang 2023',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="de">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
