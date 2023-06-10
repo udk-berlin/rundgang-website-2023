2ba20578bb8a260c97d36a14b1ca9d15547fabdb
Lukas Esser <lukasesser@hotmail.com>
2023-06-10 02:13:28 +0200

    carousel infos

--- src/components/pages/program/info_grid.js
+++ src/components/pages/program/info_grid.js
@@ -2,11 +2,13 @@ import styled from "styled-components";
 import InfoGridDate from "./info_grid_date";
 import InfoGridTime from "./info_grid_time";
 import { InfoGridEvent } from "./info_grid_event";
+import { InfoGridContext, InfoGridLocation } from "./info_grid_cards";
+import { useRef } from "react";
 
 const Container = styled.div`
   display: grid;
-  row-gap: 0.75rem;
   justify-items: start;
+  row-gap: 0.75rem;
   font-size: 0.7rem;
   font-weight: 500;
 
@@ -20,14 +22,50 @@ const Container = styled.div`
   }
 `;
 
+const ScrollableContainer = styled(Container)`
+  width: 100%;
+  grid-template-columns: repeat(3, 100%);
+  align-items: start;
+  overflow-x: auto;
+  scroll-snap-type: x mandatory;
+  padding-bottom: 2px;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  & > * {
+    scroll-snap-align: start;
+    scroll-snap-stop: normal;
+  }
+`;
+
 export default function InfoGrid() {
   return (
     <Container>
       <InfoGridEvent EventType="Tanz"></InfoGridEvent>
-      <div>
-        <InfoGridDate Day="3" />
-        <InfoGridTime begin="10:00" end="20:00" />
-      </div>
+      <ScrollableContainer>
+        <div>
+          <InfoGridDate Day="3" />
+          <InfoGridTime begin="10:00" end="20:00" />
+        </div>
+        <InfoGridLocation />
+        <InfoGridContext />
+      </ScrollableContainer>
     </Container>
   );
 }
+
+// const ScrollDemo = () => {
+//   const myRef = useRef(null)
+
+//   const executeScroll = () => myRef.current.scrollIntoView()
+//   // run this function from an event handler or an effect to execute scroll
+
+//   return (
+//      <>
+//         <div ref={myRef}>Element to scroll to</div>
+//         <button onClick={executeScroll}> Click to scroll </button>
+//      </>
+//   )
+// }
---
+++ src/components/pages/program/info_grid_cards.js
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-items: start;
+`;
+
+const ItemWrapper = styled.div`
+  margin-top: var(--info-border-width);
+  display: inline;
+  outline: var(--info-border-width) solid var(--info-border-color);
+  margin-left: ${(props) => props.margin};
+
+  & > div {
+    padding: 0.2rem 0.4rem;
+  }
+`;
+
+const Item = ({ margin, children }) => {
+  return (
+    <ItemWrapper margin={margin}>
+      <div>{children}</div>
+    </ItemWrapper>
+  );
+};
+
+export const InfoGridLocation = ({ prop }) => {
+  return (
+    <Container>
+      <Item margin="170px">Hardenbergstr. 33</Item>
+      <Item margin="10px">Etage: 2</Item>
+      <Item margin="50px">Raum: 243/244</Item>
+    </Container>
+  );
+};
+
+export const InfoGridContext = ({ prop }) => {
+  return (
+    <Container>
+      <Item margin="200px">Fakultät Bildende Künste</Item>
+      <Item margin="150px">Institut für Kunst</Item>
+      <Item margin="100px">Malerei / Zeichnen</Item>
+      <Item margin="50px">Fachklasse Streuli</Item>
+    </Container>
+  );
+};
