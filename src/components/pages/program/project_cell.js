4beb9ef2d26456768e9ea7e83bddd41409f88018
marisanest <marisanest@mailbox.org>
2023-06-15 02:53:04 +0200

    fix program grid.

--- src/components/pages/program/project_cell.js
+++ src/components/pages/program/project_cell.js
@@ -1,5 +1,3 @@
-import styled from 'styled-components'
-
 import ProjectPageTitle from '@/components/pages/project/project_title'
 import ProjectPageAuthors from '@/components/pages/project/project_authors'
 import InfoGrid from '@/components/pages/program/info_grid'
@@ -7,25 +5,55 @@ import { LocalizedLink } from "@/components/localization/links";
 
 const authors = ['Marisa Nest', 'Juan Pablo Gaviria Bedoya', 'Lukas Esser']
 
-export default function ProjectCell ({ src }) {
+export default function ProjectCell ({ event }) {
   return (
-    <Container>
-      <LocalizedLink href="/project">
-        <img src={src}></img>
-      </LocalizedLink>
-      <LocalizedLink href="/project">
-        <ProjectPageTitle fontSize={1}>
-          REM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD
-        </ProjectPageTitle>
-      </LocalizedLink>
-      <ProjectPageAuthors fontSize={0.8} authors={authors} />
-      <InfoGrid eventType="Tanz" />
-    </Container>
+    <div key={event.src}>
+      <EventImage event={event}/>
+      <EventTitle event={event}/>
+      <EventAuthors event={event} />
+      <EventInfos event={event} />
+    </div>
+  )
+}
+function EventImage ({ event }) {
+  return (
+    <EventLink event={event}>
+      <img
+        src={event.src}
+        alt={event.name}
+        loading="lazy"
+        style={{width: '100%'}}
+      />
+    </EventLink>
   )
 }
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 0.75rem;
-`
+function EventTitle ({ event }) {
+  return (
+    <EventLink event={event}>
+      <ProjectPageTitle fontSize={1}>
+        REM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT, SED DO EIUSMOD
+      </ProjectPageTitle>
+    </EventLink>
+  )
+}
+
+function EventAuthors ({ event }) {
+  return (
+    <ProjectPageAuthors fontSize={0.8} authors={authors} />
+  )
+}
+
+function EventInfos({ event }) {
+  return (
+    <InfoGrid eventType="Tanz" />
+  )
+}
+
+function EventLink ({ event, children }) {
+  return (
+    <LocalizedLink href={`/event/${event.id}`}>
+      {children}
+    </LocalizedLink>
+  )
+}
--- src/pages/program.js
+++ src/pages/program.js
@@ -1,77 +1,10 @@
-import styled from 'styled-components'
-
 import Page from "@/components/page/page";
-import ProjectCell from '@/components/pages/program/project_cell'
-import Layout from "@/components/layout/layout";
+import Program from "@/components/pages/program/program";
 
 export default function ProgramPage () {
   return (
     <Page>
-      <Layout>
-        <Grid>
-          <ProjectCell src={'https://placehold.co/900x400'} />
-          <ProjectCell src={'https://placehold.co/300x250'} />
-          <ProjectCell src={'https://placehold.co/900x400'} />
-          <ProjectCell src={'https://placehold.co/500x900'} />
-          <ProjectCell src={'https://placehold.co/900x500'} />
-          <ProjectCell src={'https://placehold.co/910x400'} />
-          <ProjectCell src={'https://placehold.co/920'} />
-          <ProjectCell src={'https://placehold.co/900'} />
-          <ProjectCell src={'https://placehold.co/300'} />
-          <ProjectCell src={'https://placehold.co/900x400'} />
-          <ProjectCell src={'https://placehold.co/400x900'} />
-          <ProjectCell src={'https://placehold.co/900x500'} />
-          <ProjectCell src={'https://placehold.co/910'} />
-          <ProjectCell src={'https://placehold.co/920'} />
-          <ProjectCell src={'https://placehold.co/900'} />
-          <ProjectCell src={'https://placehold.co/300'} />
-          <ProjectCell src={'https://placehold.co/900x400'} />
-          <ProjectCell src={'https://placehold.co/400x900'} />
-          <ProjectCell src={'https://placehold.co/900x500'} />
-          <ProjectCell src={'https://placehold.co/910'} />
-          <ProjectCell src={'https://placehold.co/920'} />
-
-          <Break />
-          <Break />
-          <Break />
-        </Grid>
-      </Layout>
+     <Program />
     </Page>
   )
 }
-
-const Grid = styled.div`
-  display: flex;
-  flex-flow: column wrap;
-  row-gap: 1rem;
-  column-gap: 0.5rem;
-  padding: 1rem;
-
-  height: 4000px;
-
-  & > * {
-    width: 24%;
-  }
-
-  & > *:nth-of-type(4n + 1) {
-    order: 1;
-  }
-  & > *:nth-of-type(4n + 2) {
-    order: 2;
-  }
-  & > *:nth-of-type(4n + 3) {
-    order: 3;
-  }
-  & > *:nth-of-type(4n) {
-    order: 4;
-  }
-`
-
-const Break = styled.span`
-  /* Force new columns */
-  flex-basis: 100%;
-  width: 0;
-  margin: 0;
-  content: "";
-  padding: 0;
-`
