976badf2d621c7019d671f210a0c49f38b7ed6b6
Marisa Nest <marisanest@users.noreply.github.com>
2023-06-19 19:07:41 +0200

    Implementing popup filter features.  (#12)

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 import Footer from "@/components/layout/footer/footer";
 import Header from "@/components/layout/header/header";
---
+++ src/components/pages/locations/map/popup/rooms.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { FormattedMessage } from 'react-intl'
+
+import styles from '@/styles/pages/locations/map/popup/Rooms.module.css'
+import { useLocation, useLocationDispatch } from '@/providers/location'
+
+export default function PopupFloors ({ location }) {
+  const locationFilter = useLocation()
+
+  if (!locationFilter.floor || locationFilter.location.id !== location.id) {
+    return <></>
+  }
+
+  const rooms = getRooms(locationFilter.floor)
+
+  return (
+    <div className={styles.roomsContainer}>
+      <PopupRoomsAll />
+      {Object.values(rooms).map((room, index) => <PopupRoom key={index} room={room} />)}
+    </div>
+  )
+}
+
+function PopupRoom ({ key, room }) {
+  const locationFilter = useLocation()
+  const dispatch = useLocationDispatch()
+  const roomSelected = ('room' in locationFilter && locationFilter.room.id === room.id)
+
+  const handleClick = (e) => {
+    dispatch(
+      {
+        type: 'select-room',
+        room: room
+      })
+  }
+
+  return (
+    <div key={key} className={[styles.roomContainer, roomSelected ? styles.roomSelected : ''].join(' ')}
+         onClick={handleClick}>
+      <div>
+        <FormattedMessage id={'room'}/>: {room.name}
+      </div>
+    </div>
+  )
+}
+
+function PopupRoomsAll () {
+  const locationFilter = useLocation()
+  const dispatch = useLocationDispatch()
+  const roomSelected = !('room' in locationFilter)
+
+  const handleClick = (e) => {
+    dispatch(
+      {
+        type: 'all-rooms'
+      })
+  }
+
+  return (
+    <div key={-1} className={[styles.roomContainer, roomSelected ? styles.roomSelected : ''].join(' ')} onClick={handleClick}>
+      <div>
+        <FormattedMessage id={'rooms.all'}/>
+      </div>
+    </div>
+  )
+}
+
+function getRooms (floor) {
+  const rooms = {}
+
+  Object.values(floor.children).forEach(child => {
+    if (child.template === 'location-room') {
+      rooms[child.id] = child
+    }
+  })
+
+  return rooms
+}
