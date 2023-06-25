d225e55b32c70210d995d727f47d09592769bda0
Lukas Esser <lukasesser@hotmail.com>
2023-06-25 15:32:02 +0200

    contact/imprint content in de/en

--- src/components/layout/static_layout.js
+++ src/components/layout/static_layout.js
@@ -1,58 +1,69 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 import { FormattedMessage } from "react-intl";
 
-export default function StaticLayout ({
-  layout = 'default',
-  title = '',
-  children
+export default function StaticLayout({
+  layout = "default",
+  title = "",
+  children,
 }) {
   return (
     <Container>
       {staticTitle(layout, title)}
       {children}
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
   display: grid;
   grid-template-columns: 1fr 7fr;
   padding: 2rem;
-`
+`;
 const StaticTitle = styled.div`
   font-weight: 600;
-`
+`;
 
 const FaqStaticTitle = styled(StaticTitle)`
   padding: 0.5rem 0;
-`
+`;
 
 const staticTitle = (layout, title) => {
   switch (layout) {
-    case 'faq':
+    case "faq":
       return (
-          <FaqStaticTitle>
-            <FormattedMessage id={title} />
-          </FaqStaticTitle>
-      )
+        <FaqStaticTitle>
+          <FormattedMessage id={title} />
+        </FaqStaticTitle>
+      );
     default:
       return (
-          <StaticTitle>
-            <FormattedMessage id={title} />
-          </StaticTitle>
-      )
+        <StaticTitle>
+          <FormattedMessage id={title} />
+        </StaticTitle>
+      );
   }
-}
+};
 
-export function StaticLayoutParagraph ({ content }) {
+export function StaticLayoutParagraph({ content }) {
   return (
     <p>
       {content.titles.map((title) => (
-        <div>{title}</div>
+        <StaticParagraphTitle>{title}</StaticParagraphTitle>
       ))}
-      {content.texts.map((texts) => (
-        <div>{texts}</div>
+      {/* {console.log(content.texts)} */}
+      {content.texts?.map((text) => (
+        <div>
+          {text.link ? (
+            <a href={text.link}>{text.text}</a>
+          ) : (
+            <div>{text.text}</div>
+          )}
+        </div>
       ))}
     </p>
-  )
+  );
 }
+
+const StaticParagraphTitle = styled.div`
+  font-weight: 500;
+`;
