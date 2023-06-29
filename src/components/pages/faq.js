63dfce2327c6701b22437c0f1aecbbd17b07f2b0
Marisa Nest <marisanest@users.noreply.github.com>
2023-06-29 12:30:25 +0200

    Adding environment variables. (#28)

--- src/components/pages/faq.js
+++ src/components/pages/faq.js
@@ -25,7 +25,7 @@ export default function Faq () {
         faqs = FAQS.en;
 
     return (
-      <Layout>
+      <Layout disableFilter={true}>
           <StaticLayout layout={'faq'} title={'faq'}>
               <Accordion items={faqs} />
           </StaticLayout>
--- src/components/pages/imprint.js
+++ src/components/pages/imprint.js
@@ -139,7 +139,7 @@ export default function Imprint() {
   if (language.locale === "en" && "en" in IMPRINT) imprint = IMPRINT.en;
 
   return (
-    <Layout>
+    <Layout disableFilter={true}>
       <StaticLayout title={"imprint"}>
         <div>
           {imprint.map((imprint) => (
