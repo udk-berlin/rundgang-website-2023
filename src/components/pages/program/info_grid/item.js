957aed2e4f2f3db2d4d6a32cb8ffe3aa3d9c6a87
Lukas Esser <lukasesser@hotmail.com>
2023-07-01 09:40:10 +0200

    adjust div width on line break

--- src/components/pages/program/info_grid/item.js
+++ src/components/pages/program/info_grid/item.js
@@ -1,7 +1,28 @@
 import { HoverLink } from "@/components/hover_link";
+import { useEffect, useRef } from "react";
 import styled from "styled-components";
 
-export default function InfoGridItemLink({className, href, children, margin,}) {
+const InfoGridItemLinkContainer = styled.div`
+  background-color: var(--color-white);
+  color: #000;
+
+  outline: var(--info-border-width) solid var(--info-border-color);
+  margin-top: var(--info-border-width);
+  margin-left: var(--info-border-width);
+
+  margin-left: ${(props) => props.margin};
+`;
+
+const InfoGridItemContainer = styled(InfoGridItemLinkContainer)`
+  padding: 0.2rem 0.4rem;
+`;
+
+export default function InfoGridItemLink({
+  className,
+  href,
+  children,
+  margin,
+}) {
   return (
     <InfoGridItemLinkContainer className={className} margin={margin}>
       <HoverLink href={href}>{children}</HoverLink>
@@ -17,18 +38,25 @@ export function InfoGridItem({ className, children, margin }) {
   );
 }
 
-const InfoGridItemLinkContainer = styled.div`
-  background: var(--color-white);
-  color: var(--color-black);
+export function InfoGridCardItem({ className, children, margin }) {
+  const inlineRef = useRef(null);
+  const borderRef = useRef(null);
+  useEffect(() => {
+    borderRef.current.style.width = inlineRef.current.offsetWidth + 20 + "px";
+  }, []);
+  return (
+    <InfoGridItemContainer
+      ref={borderRef}
+      className={className}
+      margin={margin}
+    >
+      <div style={{ display: "inline" }} ref={inlineRef}>
+        {children}
+      </div>
+    </InfoGridItemContainer>
+  );
+}
 
-  outline: var(--info-border-width) solid var(--info-border-color);
-  margin-top: var(--info-border-width);
-  margin-left: var(--info-border-width);
-
-  margin-left: ${({ margin }) => margin};
-`;
-
-const InfoGridItemContainer = styled(InfoGridItemLinkContainer)`
-  padding: var(--info-grid-padding);
-`;
-
+function convertRemToPixels(rem) {
+  return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
