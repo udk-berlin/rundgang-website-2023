3d16691bf2ab956970d622c59e39b9eac7d5ba05
Lukas Esser <lukasesser@hotmail.com>
2023-07-02 13:49:11 +0200

    program page

---
+++ src/themes/pages/program.js
@@ -0,0 +1,19 @@
+import { baseLTheme, baseMTheme } from "@/themes/theme";
+
+export const programLTheme = {
+  ...baseLTheme,
+  MASONRY_COLUMNS: 4,
+  MASONRY_GUTTER: "0.75rem",
+};
+
+export const programMTheme = {
+  ...baseMTheme,
+  MASONRY_COLUMNS: 3,
+  MASONRY_GUTTER: "0.75rem",
+};
+
+export const programSTheme = {
+  ...baseMTheme,
+  MASONRY_COLUMNS: 2,
+  MASONRY_GUTTER: "0.75rem",
+};
