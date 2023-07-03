55e870551cde1d725d025acc547232529e7eb73c
Marisa Nest <marisanest@users.noreply.github.com>
2023-07-03 20:29:15 +0200

    Landing page adjustments and mobile implementation. (#42)

---
+++ src/components/pages/landing/landing.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from 'react'
+
+import { ThemeProvider } from "styled-components";
+import LandingContent from "@/components/pages/landing/content";
+
+import LandingBackground from "@/components/pages/landing/background";
+
+import useWindowSize from "@/hooks/window_size";
+import { landingLTheme, landingMTheme, breakpoints } from "@/themes/pages/landing";
+
+export default function Landing () {
+  const [responsiveTheme, setResponsiveTheme] = useState(landingLTheme)
+  const windowSize = useWindowSize()
+
+  useEffect(() => {
+    if (windowSize.width <= breakpoints.m) {
+      setResponsiveTheme(landingMTheme)
+    } else {
+      setResponsiveTheme(landingLTheme)
+    }
+  }, [windowSize.width])
+
+  return (
+    <ThemeProvider theme={responsiveTheme}>
+      <LandingBackground />
+      <LandingContent />
+    </ThemeProvider>
+  )
+}
---
+++ src/themes/pages/landing.js
@@ -0,0 +1,81 @@
+import { baseLTheme, baseMTheme } from "@/themes/theme";
+
+export const breakpoints = {
+  m: 700,
+};
+
+export const landingLTheme = {
+  ...baseLTheme,
+  id: 'l',
+  fontWeights: {
+    s: 400
+  },
+  localization: {
+    fontSize: '1rem',
+  },
+  header: {
+    gridTemplateColumns: '1fr 1fr 1fr',
+    gridTemplateRows: '0',
+    padding: '0',
+    fontSize: '1rem',
+    timeline: {
+      justifySelf: 'end',
+    }
+  },
+  footer: {
+    gridTemplateColumns: '1fr 1fr 1fr',
+    gridTemplateRows: '0',
+    padding: '0',
+    fontSize: '1rem',
+    imprint: {
+      justifySelf: 'center',
+      paddingTop: '0',
+    },
+    faq: {
+      justifySelf: 'end',
+      paddingTop: '0',
+    },
+  },
+  info: {
+    alignItems: 'center',
+    fontSize: '1.2rem'
+  }
+};
+
+export const landingMTheme = {
+  ...baseMTheme,
+  id: 'm',
+  fontWeights: {
+    s: 400
+  },
+  localization: {
+    fontSize: '1rem',
+  },
+  header: {
+    gridTemplateColumns: '1fr',
+    gridTemplateRows: '1fr 1fr',
+    padding: '1rem 1rem 0 1rem',
+    fontSize: '1.3rem',
+    timeline: {
+      justifySelf: 'end',
+    }
+  },
+  footer: {
+    gridTemplateColumns: '1fr',
+    gridTemplateRows: '1fr 1fr 1fr',
+    padding: '0 1rem 1rem 1rem',
+    fontSize: '0.95rem',
+    imprint: {
+      justifySelf: 'start',
+      paddingTop: '0.05rem',
+    },
+    faq: {
+      justifySelf: 'start',
+      paddingTop: '0.25rem',
+    },
+  },
+  info: {
+    alignItems: '',
+    fontSize: '1rem'
+  }
+};
