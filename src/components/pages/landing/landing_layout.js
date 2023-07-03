1a0d308bdcd584c8da32927dc5b50ba6db27d7b5
marisanest <marisanest@mailbox.org>
2023-07-03 15:50:56 +0200

    Landing page menu svg integration.

--- src/components/pages/landing/landing_layout.js
+++ src/components/pages/landing/landing_layout.js
@@ -2,14 +2,13 @@ import styles from '@/styles/pages/landing/LandingLayout.module.css'
 
 import FooterLanding from './landing_footer'
 import HeaderLanding from './landing_header'
-import LandingMenu from './landing_menu'
-import LandingMenu2 from "@/components/pages/landing/landing_menu_2";
+import LandingMenu from "@/components/pages/landing/landing_menu";
 
 export default function LandingLayout () {
   return (
     <div className={styles.container}>
       <HeaderLanding />
-      <LandingMenu2 />
+      <LandingMenu />
       <FooterLanding />
     </div>
   )
