c5242179819077e256d0849893c794f405058080
marisanest <marisanest@mailbox.org>
2023-07-03 15:10:10 +0200

    Landing page adjustments.

--- src/components/pages/landing/landing_layout.js
+++ src/components/pages/landing/landing_layout.js
@@ -3,12 +3,13 @@ import styles from '@/styles/pages/landing/LandingLayout.module.css'
 import FooterLanding from './landing_footer'
 import HeaderLanding from './landing_header'
 import LandingMenu from './landing_menu'
+import LandingMenu2 from "@/components/pages/landing/landing_menu_2";
 
 export default function LandingLayout () {
   return (
     <div className={styles.container}>
       <HeaderLanding />
-      <LandingMenu />
+      <LandingMenu2 />
       <FooterLanding />
     </div>
   )
